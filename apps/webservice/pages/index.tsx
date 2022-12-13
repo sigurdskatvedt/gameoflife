07cf746e257856c8e286d86bad415caba0db6f41
Sigurd Skatvedt <s.skatvedt@gmail.com>
2022-12-13 14:15:12 +0100

    add flowbite

--- apps/webservice/pages/index.tsx
+++ apps/webservice/pages/index.tsx
@@ -1,5 +1,5 @@
-export default function Home() {
-  return (
-    <h1 className="from-neutral-900 text-9xl font-extrabold">Hello World</h1>
-  )
+import { UiNavbar } from "ui"
+
+export default function MyPage() {
+  return <UiNavbar></UiNavbar>
 }
