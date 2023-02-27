105fb3bd2e356ead4e76b19a8b31fb85fa5e7e03
Sigurd Skatvedt <s.skatvedt@gmail.com>
2023-02-27 16:30:44 +0100

    change to new nextjs 13.2 metadata

--- apps/webservice/app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Rule Maker</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </>
-  )
-}
--- apps/webservice/app/page.tsx
+++ apps/webservice/app/page.tsx
@@ -1,8 +1,14 @@
+import type { Metadata } from "next"
 import { UiTable } from "ui"
 import { UiNavbar } from "ui/Navbar"
 
 // force this page to be dynamically re-rendered each time as it depends on from it's the api server
 export const dynamic = "force-dynamic"
+
+export const metadata: Metadata = {
+  title: "Game of Life Dashboard",
+  description: "Here you can manage the rules for Game of Life",
+}
 
 export default function Page() {
   return (
